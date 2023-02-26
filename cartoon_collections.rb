ee91881da9b29e082d6afd7c3844229f7899a2b3
jay <jaypifiverr@gmail.com>
2023-02-26 10:49:07 -0500

    commit tree 43031c0339e29fc9a259e30ff67153e5c9424590

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+a = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+short_words = ["puff", "go", "two"]
+
+ingredients = ["garlic", "rosemary", "bread"]
+snacks = ["crackers", "gouda", "thyme"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+
+def roll_call_dwarves(array)
   # Your code here
+  array.map.with_index(1) do |name, index| 
+    print "#{index}. #{name} \n"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |change|
+    change.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |words|
+    words.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |t|
+    cheese_types.include?(t)
+  end
 end
+
+binding.pry
